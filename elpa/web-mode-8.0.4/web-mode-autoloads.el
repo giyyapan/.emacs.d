f2ba3416d45e0f5b37cb4f4f3ea2edd0842195ff
giyyapan <giyyapan@gmail.com>
2015-05-23 16:12:05 +0800

    update

--- elpa/web-mode-8.0.4/web-mode-autoloads.el
+++ elpa/web-mode-8.0.4/web-mode-autoloads.el
@@ -3,8 +3,8 @@
 ;;; Code:
 
 
-;;;### (autoloads (web-mode) "web-mode" "web-mode.el" (21584 26245
-;;;;;;  96758 575000))
+;;;### (autoloads (web-mode) "web-mode" "web-mode.el" (21725 41068
+;;;;;;  319607 645000))
 ;;; Generated autoloads from web-mode.el
 
 (autoload 'web-mode "web-mode" "\
@@ -14,8 +14,8 @@ Major mode for editing web templates (HTML documents with embedded parts and blo
 
 ;;;***
 
-;;;### (autoloads nil nil ("web-mode-pkg.el") (21584 26245 183649
-;;;;;;  432000))
+;;;### (autoloads nil nil ("web-mode-pkg.el") (21725 41068 428154
+;;;;;;  772000))
 
 ;;;***
 
